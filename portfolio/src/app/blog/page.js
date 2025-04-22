e130a2186c2379753600bb9c8e517d6350184757
Yogesh <yogeshsamkumar@gmail.com>
2025-04-21 20:18:22 -0400

    centering a div

--- portfolio/src/app/blog/page.js
+++ portfolio/src/app/blog/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Blog() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center mt-20">
       <h1 className="text-[#cfc9c2]">coming soon</h1>
     </div>
   );
